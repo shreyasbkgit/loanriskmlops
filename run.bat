7e485c6953fcdb92686322c2f2fdeb97efd596f0
shreyas <shreyasbhatk.scs24@bmsce.ac.in>
2025-07-20 07:41:58 +0530

    Final MLOps pipeline with DVC stages and metrics

--- run.bat
+++ run.bat
@@ -1,19 +1,23 @@
 @echo off
 CALL conda activate loanmlops
 
+<<<<<<< HEAD
 echo [🚀] Starting Loan Risk ML Pipeline...
 
 pip install pandas scikit-learn
 
 =======
+=======
+>>>>>>> bc29a3b (Final MLOps pipeline with DVC stages and metrics)
 echo [🚀] Starting Loan Risk ML Pipeline...
 
-
-REM Step 1: Install dependencies globally (not recommended long-term)
 pip install pandas scikit-learn
 
+<<<<<<< HEAD
 REM Step 2: Run scripts
 >>>>>>> e7475f2 (dvc)
+=======
+>>>>>>> bc29a3b (Final MLOps pipeline with DVC stages and metrics)
 python src\preprocess.py data\train.csv data\clean_train.csv
 python src\train.py data\clean_train.csv models\model.pkl
 python src\evaluate.py data\clean_train.csv models\model.pkl
